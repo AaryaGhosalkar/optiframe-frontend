9b818f16f0723cb29f6ce6ce8fce5b74a419b891
Aarya Ghosalkar <aaryaghosalkar19@gmail.com>
2026-02-24 00:55:49 +0530

    added role

--- src/pages/TryOn.jsx
+++ src/pages/TryOn.jsx
@@ -3,7 +3,7 @@ import { useLocation } from "react-router-dom";
 import { FaceMesh } from "@mediapipe/face_mesh";
 import { Camera } from "@mediapipe/camera_utils";
 
-const API_URL = "https://optiframe-backend.onrender.com";
+const API_URL = "https://optiframe-backend.onrender.com/api";
 
 export default function TryOn() {
   const videoRef = useRef(null);
